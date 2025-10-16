6bddc42d25efbfeb2fce55c5f0c6c9fbe18dc5f9
ChristianMorland <christian.morland@thuenen.de>
2025-10-16 16:08:40 +0200

    centralize all paths within the model

--- TiMBA/main.py
+++ TiMBA/main.py
@@ -1,24 +1,32 @@
 from TiMBA.main_runner.main_runner import main
 from TiMBA.data_management.ParameterCollector import ParameterCollector
-from TiMBA.parameters import INPUT_WORLD_PATH
+from TiMBA.parameters import INPUT_WORLD_PATH, DATA_FOLDER
 from pathlib import Path
 import datetime as dt
 import os
 import warnings
+import sys
 warnings.simplefilter(action='ignore', category=FutureWarning)
+from TiMBA.user_io.default_parameters import user_input
+Parameters = ParameterCollector(user_input=user_input)
 
-PACKAGEDIR = Path(__file__).parent.absolute()
-
-if __name__ == '__main__':
-    from TiMBA.user_io.default_parameters import user_input
-    Parameters = ParameterCollector(user_input=user_input)
-    world_list = os.listdir(INPUT_WORLD_PATH)
+def run_timba(Parameters=Parameters,folderpath:str=None):
+    PACKAGEDIR = Path(__file__).parent.absolute()
+    if folderpath is None:
+            folderpath = PACKAGEDIR
+    INPUT_PATH = folderpath / DATA_FOLDER / INPUT_WORLD_PATH
+    try:
+        world_list = os.listdir(INPUT_PATH)
+    except FileNotFoundError:
+        print("FileNotFoundError at: ",INPUT_PATH)
+        print(f"Make sure input data is downloaded to {INPUT_PATH} \nor change the folder path where the data is stored.")
+        sys.exit(1)
     for world in world_list:
         current_dt = dt.datetime.now().strftime("%Y%m%dT%H-%M-%S")
-        print(f"The model starts now:", (dt.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")),"\n")
-        print(f"Path:", INPUT_WORLD_PATH)
-        print(f"Name of input file:", world[:len(world) - 5], "\n")
-        print(f"User input for model settings:\n",
+        print("The model starts now:", (dt.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")),"\n")
+        print(f"Path: {folderpath}")
+        print(f"Name of input file: {world[:len(world) - 5]} \n")
+        print("User input for model settings:\n",
               f"Start year: {Parameters.year}\n",
               f"Number of periods: {Parameters.max_period}\n",
               f"Calculation of prices by: {Parameters.calc_product_prices}\n",
@@ -30,11 +38,13 @@ if __name__ == '__main__':
               f"Optimization gives verbose logs: {Parameters.verbose_optimization_logger}\n",
               f"TiMBA gives verbose logs: {Parameters.verbose_calculation_logger}\n",
               f"Read additional informations: {Parameters.addInfo}\n")
-
         main(UserIO=Parameters,
              world_version=world,
              time_stamp=current_dt,
-             package_dir=PACKAGEDIR,
-             sc_name=world[:len(world) - 5])
+             Data_Path=folderpath / DATA_FOLDER,
+             sc_name=world[:len(world) - 5])     
+
+if __name__ == '__main__':
+     run_timba(Parameters=Parameters)#,folderpath=Path(r"E:/"))
 
 
