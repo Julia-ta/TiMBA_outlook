dc315a241463e7fc8a6dfd235651e52b6d37eab0
Christian Morland <christian.morland@thuenen.de>
2025-12-03 17:08:50 +0100

    change main.py to be in line with PEP8

--- TiMBA/main.py
+++ TiMBA/main.py
@@ -1,21 +1,20 @@
 from TiMBA.main_runner.main_runner import main
 from TiMBA.parameters import INPUT_WORLD_PATH, DATA_FOLDER
 from TiMBA.logic.model_extensions import run_extensions
-from TiMBA.data_management.ParameterCollector import ParameterCollector
 from pathlib import Path
 import datetime as dt
 import os
 import warnings
-import sys
-warnings.simplefilter(action='ignore', category=FutureWarning)
 from TiMBA.results_logging.base_logger import close_logger
 from TiMBA.parameters.paths import (
-    DATA_FOLDER, GIT_USER, GIT_REPO, GIT_BRANCH,
-    GIT_FOLDER, INPUT_WORLD_PATH, DESTINATION_PATH
+    GIT_USER, GIT_REPO, GIT_BRANCH,
+    GIT_FOLDER, DESTINATION_PATH
 )
 from TiMBA.data_management.Load_Data import load_data
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-def run_timba(Parameters:dict=None,folderpath:str=None):
+
+def run_timba(Parameters: dict = None, folderpath: str = None):
     if Parameters is None:
         print("Paramerters need to be set before running TiMBA.",
               "Note: For this simulation standard parameters are used from, ",
@@ -23,15 +22,16 @@ def run_timba(Parameters:dict=None,folderpath:str=None):
         Parameters = parameter_setter()
     PACKAGEDIR = Path(__file__).parent.parent.absolute()
     if folderpath is None:
-            folderpath = PACKAGEDIR
+        folderpath = PACKAGEDIR
     INPUT_PATH = folderpath / DATA_FOLDER / INPUT_WORLD_PATH
     if os.path.exists(INPUT_PATH):
         pass
     else:
-        print("FileNotFoundError at: ",INPUT_PATH)
+        print("FileNotFoundError at: ", INPUT_PATH)
         print(f"Make sure input data is downloaded to {INPUT_PATH} \nor ",
               "change the folder path where the data is stored.",
-              "Note: For this simulation standard input files will be loaded from GitHub",
+              """Note: For this simulation standard input
+              files will be loaded from GitHub""",
               f"and saved at {folderpath / DESTINATION_PATH}")
         load_data(
             user=GIT_USER,
@@ -43,36 +43,41 @@ def run_timba(Parameters:dict=None,folderpath:str=None):
     world_list = os.listdir(INPUT_PATH)
     for world in world_list:
         current_dt = dt.datetime.now().strftime("%Y%m%dT%H-%M-%S")
-        print("The model starts now:", (dt.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")),"\n")
+        print("The model starts now:",
+              (dt.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")),
+              "\n")
         print(f"Path: {folderpath}")
         print(f"Name of input file: {world[:len(world) - 5]} \n")
         print("User input for model settings:\n",
-              f"Start year: {Parameters.year}\n",
-              f"Number of periods: {Parameters.max_period}\n",
-              f"Calculation of prices by: {Parameters.calc_product_prices}\n",
-              f"Calculation of world prices by: {Parameters.calc_world_prices}\n",
-              f"Material balance: {Parameters.material_balance}\n",
-              f"Input data through serialization: {Parameters.serialization}\n",
-              f"Dynamization activated: {Parameters.dynamization_activated}\n",
-              f"Prices are capped: {Parameters.capped_prices}\n",
-              f"Optimization gives verbose logs: {Parameters.verbose_optimization_logger}\n",
-              f"TiMBA gives verbose logs: {Parameters.verbose_calculation_logger}\n",
-              f"Read additional informations: {Parameters.addInfo}\n")
+              f"""Start year: {Parameters.year}\n
+              Number of periods: {Parameters.max_period}\n
+              Calculation of prices by: {Parameters.calc_product_prices}\n
+              Calculation of world prices by: {Parameters.calc_world_prices}\n
+              Material balance: {Parameters.material_balance}\n",
+              Input data through serialization: {Parameters.serialization}\n
+              Dynamization activated: {Parameters.dynamization_activated}\n
+              Prices are capped: {Parameters.capped_prices}\n
+              Optimization gives verbose logs:
+              {Parameters.verbose_optimization_logger}\n
+              TiMBA gives verbose logs:
+              {Parameters.verbose_calculation_logger}\n
+              Read additional informations: {Parameters.addInfo}\n""")
         main(UserIO=Parameters,
              world_version=world,
              time_stamp=current_dt,
              Data_Path=folderpath / DATA_FOLDER,
-             sc_name=world[:len(world) - 5])     
+             sc_name=world[:len(world) - 5])
         close_logger()
-    
+
+
 def parameter_setter():
     from TiMBA.data_management.ParameterCollector import ParameterCollector
     from TiMBA.user_io.default_parameters import user_input
     return ParameterCollector(user_input=user_input)
 
+
 if __name__ == '__main__':
     Parameters = parameter_setter()
-    run_timba(Parameters=Parameters)#,folderpath=Path(r"E:/"))
+    run_timba(Parameters=Parameters)  # ,folderpath=Path(r"E:/"))
 
     run_extensions(UserIO=Parameters)
-
