82793bad89df5ba8a588e04dd463cce04034bb1f
Christian Morland <christian.morland@thuenen.de>
2025-12-04 07:57:18 +0100

    normalized project name from TiMBA to timba

--- TiMBA/main.py
+++ TiMBA/main.py
@@ -78,4 +78,4 @@ if __name__ == '__main__':
     Parameters = parameter_setter()
     run_timba(Parameters=Parameters)  # ,folderpath=Path(r"E:/"))
 
-    run_extensions(UserIO=Parameters)
+    # run_extensions(UserIO=Parameters)
